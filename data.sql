f1fb8695e25e998a6fb3a66efc79c994978cd6b9
SergeyBIBO <serw21a@mail.ru>
2021-12-22 18:25:36 +0300

    adding new files

---
+++ data.sql
@@ -0,0 +1,11 @@
+insert into usr (first_name, middle_name, last_name, email) values
+('Козлова', 'Александра', 'Андреевна', 'kozlova@my.example.com'),
+('Князев', 'Матвей', 'Константинович', 'knyazyev@my.example.com'),
+('Егорова', 'Анна', 'Захаровна', 'egorovna@my.example.com'),
+('Лавров', 'Глеб', 'Глебович', 'lavrov@my.example.com'),
+('Ульянова', 'Таисия', 'Артуровна', 'ulyanova@my.example.com'),
+('Кузнецов', 'Дмитрий', 'Константинович', 'kuznecov@my.example.com'),
+('Большаков', 'Давид', 'Александрович', 'bolshakov@my.example.com'),
+('Федеров', 'Дмитрий', 'Егорович', 'fedorov@my.example.com'),
+('Носова', 'Мария', 'Матвеевна', 'nosova@my.example.com'),
+('Белов', 'Александр', 'Матвеевич', 'bellove@my.example.com');
