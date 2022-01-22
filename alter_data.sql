3c272d95a11556264d3a51383911989391ad04f3
SergeyBIBO <serw21a@mail.ru>
2022-01-22 19:05:49 +0300

    edit files

--- alter_data.sql
+++ alter_data.sql
@@ -4,3 +4,9 @@ alter table usr alter e_mail set not null
 alter table usr add column birth_date timestamp;
 update usr set birth_date='2004-01-08 04:05:06' where id<=10;
 update usr set birth_date='2008-01-08 04:05:06' where id>10;
+alter table usr add constraint FK_USR_ROLE foreign key (role_name) references
+role;
+alter table role add constraint FK_ROLE_PERMISSION foreign key (name) references
+permission;
+alter table role_permission add constraint FK_ROLE_PERMISSION_PERMISSION foreign key (role_name) references
+permission;
